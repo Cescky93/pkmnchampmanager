a6f149cd0dc8896bc325fce9b05c9a5155c99280
Cescky93 <cesckyc14@gmail.com>
2026-07-07 10:44:23 +0200

    v0.4

--- sw.js
+++ sw.js
@@ -1,4 +1,26 @@
-const CACHE = 'champions-manager-v03';
-const ASSETS = ['./','./index.html','./styles.css','./data.js','./app.js','./manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE_NAME = "pkmnchampmanager-v04";
+const ASSETS = ["./", "./index.html", "./styles.css", "./data.js", "./app.js", "./manifest.json"];
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith((async () => {
+    try {
+      const fresh = await fetch(event.request);
+      const cache = await caches.open(CACHE_NAME);
+      cache.put(event.request, fresh.clone());
+      return fresh;
+    } catch {
+      return caches.match(event.request);
+    }
+  })());
+});
